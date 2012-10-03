7a43f7ecd95522eed966a7515689346b5fe481cb
Mick Trujillo <mick1157@gmail.com>
2012-10-03 08:39:04 -0600

    Initialize project

---
+++ app/models/email_id.rb
@@ -0,0 +1,3 @@
+class EmailId < ActiveRecord::Base
+   attr_accessible :email_id, :user_name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microblog::Application.config.secret_token = 'e14c3fd07e1f7ab773a830a7b8ab34a9efd255953603309b649d52eef762a6a477f0c2df8557928b7dbbcb6f418211549d744e4f6746b0b65bed917be70083f4'
